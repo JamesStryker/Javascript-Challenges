85b69e47f064edd36af72268dea14ffae89d50ec
James Stryker Mendez <Jamessmendez@gmail.com>
2021-04-19 13:13:54 -0400

    finished guessing game dom

--- challenges/Developer_Skills _Editor_Setup_challenge/index.js
+++
@@ -1,9 +0,0 @@
-let data1 = [17, 21, 23];
-let data2 = [12,5,-5,0,4]; 
-
-function printForecast(arr){
-    let str = "";
-    for(let i=0;i<arr.length;i++){
-        str += `... ${arr[i]}deg C in ${i + 1} days`
-    }
-}
