b8d985fbedf922c0b6740e44ddc0a48baa8c5a93
James Stryker Mendez <Jamessmendez@gmail.com>
2021-04-21 12:22:58 -0400

    created named function for closed modal
    made available for clicking close btn and overlay

--- challenges/DOM/Modal_popUp_project/index.js
+++ challenges/DOM/Modal_popUp_project/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const showModal = document.getElementsByClassName("show-modal");
+const closeModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+
+for(let i = 0;i<showModal.length;i++){
+    showModal[i].addEventListener('click',  function(){
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    })
+}
+
+const close = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+closeModal.addEventListener("click", close);
+overlay.addEventListener("click", close);
+
+
